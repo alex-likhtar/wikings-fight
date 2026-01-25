c824b6c81db6dcf9b9a5c2a666767864f864e2f1
alex-lihtar <alexlikhtar@icloud.com>
2026-01-25 18:27:19 +0300

    починка анимации атаки1 и 2 ;смерти;комбо дабавлен магазин (доделать)

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,63 +2,114 @@ extends CharacterBody2D
 
 enum {
 	IDLE,
-	ATACK1,
-	ATACK2,
+	ATTACK1,
+	ATTACK2,
 	MOVE,
 	BLOCK,
-	SLIDE
+	SLIDE,
+	DEATH # Добавили новое состояние
 }
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
 var gravity = ProjectSettings.get_setting('physics/2d/default_gravity')
 
 @onready var anim = $AnimatedSprite2D
 @onready var animPlayer = $AnimationPlayer
+
 var health = 100
 var gold = 0
 var state = MOVE
-var combo = false
+
+# Переменная для запоминания следующего удара
+var next_attack = 1
+
+func _ready():
+	animPlayer.animation_finished.connect(_on_animation_finished)
+
+	# Отключаем повторы для атак и слайда
+	if animPlayer.has_animation("attack1"):
+		animPlayer.get_animation("attack1").loop_mode = Animation.LOOP_NONE
+	if animPlayer.has_animation("attack2"):
+		animPlayer.get_animation("attack2").loop_mode = Animation.LOOP_NONE
+	if animPlayer.has_animation("slide"):
+		animPlayer.get_animation("slide").loop_mode = Animation.LOOP_NONE
+
+	# ИСПРАВЛЕНИЕ: Отключаем повтор для смерти, чтобы она сыграла 1 раз и остановилась
+	if animPlayer.has_animation("death"):
+		animPlayer.get_animation("death").loop_mode = Animation.LOOP_NONE
 
 func _physics_process(delta):
+	# Глобальная проверка здоровья
+	# Если здоровье 0 и мы еще не в состоянии смерти -> переходим в смерть
+	if health <= 0 and state != DEATH:
+		state = DEATH
+
+	# Машина состояний
 	match state:
 		MOVE:
 			move_state()
-		ATACK1:
+		ATTACK1:
 			attack1_state()
-		ATACK2:
+		ATTACK2:
 			attack2_state()
 		BLOCK:
 			block_state()
 		SLIDE:
 			slide_state()
-	
-	# Гравитация
+		DEATH:
+			death_state() # Вызываем функцию смерти
+
+	# Гравитация работает всегда, даже при смерти (чтобы труп падал на землю)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	# Анимация падения (Включаем только если мы НЕ в блоке и НЕ в слайде)
+
+	# Анимация падения (только если живой и двигается)
 	if velocity.y > 0 and state == MOVE:
-		animPlayer.play("fall")  
-		
-	if health <= 0:
-		health = 0
-		animPlayer.play("death")
-		# Здесь используем await аккуратно, так как это конец игры
-		await animPlayer.animation_finished
-		queue_free()
-		get_tree().change_scene_to_file("res://menu.tscn")            
+		animPlayer.play("fall")
 
 	move_and_slide()
 
+# --- ОБРАБОТКА СИГНАЛОВ (КОНЕЦ АНИМАЦИИ) ---
+func _on_animation_finished(anim_name):
+	if anim_name == "attack1":
+		if Input.is_action_pressed("attack"):
+			state = ATTACK2
+		else:
+			state = MOVE
+			next_attack = 2
+
+	elif anim_name == "attack2":
+		# Бесконечное комбо: если кнопка зажата, бьем снова 1-й атакой
+		if Input.is_action_pressed("attack"):
+			state = ATTACK1
+		else:
+			state = MOVE
+			next_attack = 1
+
+	elif anim_name == "slide":
+		state = MOVE
+
+	# КОГДА АНИМАЦИЯ СМЕРТИ ЗАКОНЧИЛАСЬ
+	elif anim_name == "death":
+		# Удаляем персонажа и выходим в меню
+		queue_free()
+		get_tree().change_scene_to_file("res://menu.tscn")
+# ------------------------------------------------------
+
 func move_state():
-	# 1. Прыжок на ПРОБЕЛ (ui_accept)
+	if Input.is_action_just_pressed("attack"):
+		velocity.x = 0
+		if next_attack == 1:
+			state = ATTACK1
+		else:
+			state = ATTACK2
+		return
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animPlayer.play("jump")
 
-	# 3. Движение
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -68,73 +119,51 @@ func move_state():
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
 			animPlayer.play("idle")
-			
+
 	if direction == -1:
 		anim.flip_h = true
 	elif direction == 1:
-		anim.flip_h = false 
+		anim.flip_h = false
 
-	# 4. Блок и Слайд (ПРАВАЯ кнопка мыши - block)
 	if Input.is_action_just_pressed("block"):
 		if velocity.x == 0:
 			state = BLOCK
 		else:
-			start_slide() # Вызываем специальную функцию для старта слайда
+			state = SLIDE
 
-# Специальная функция для настройки слайда
 func start_slide():
-	state = SLIDE
-	animPlayer.play("slide")
-	
-	# --- ПРИНУДИТЕЛЬНОЕ ЛЕЧЕНИЕ ---
-	# Этот код программно отключает повтор анимации, 
-	# если вы забыли выключить его в редакторе.
-	if animPlayer.has_animation("slide"):
-		animPlayer.get_animation("slide").loop_mode = Animation.LOOP_NONE
-	# ------------------------------
-
-	if Input.is_action_just_pressed("attack"):
-		state = ATACK1
-
-
-
+	pass
 
 func block_state():
 	velocity.x = 0
 	if animPlayer.current_animation != "block":
 		animPlayer.play("block")
-		
-	# Если отпустили кнопку блока -> бежим дальше
 	if not Input.is_action_pressed("block"):
 		state = MOVE
-		
+
 func slide_state():
-	# 1. Если анимация закончилась -> выход
-	if not animPlayer.is_playing():
-		state = MOVE
-		return
-
-	# 2. ЗАЩИТА ОТ ЗАВИСАНИЯ:
-	# Если AnimationPlayer вдруг переключился на что-то другое (не "slide"),
-	# или имя анимации в коде не совпадает с редактором -> выход.
 	if animPlayer.current_animation != "slide":
-		state = MOVE
-
+		animPlayer.play("slide")
 
 func attack1_state():
-	if Input.is_action_just_pressed("attack") and combo == true:
-		state = ATACK2
 	velocity.x = 0
-	animPlayer.play("atack1")
-	await animPlayer.animation_finished
-	state = MOVE
-	
+	if not animPlayer.has_animation("attack1"): return
+	if animPlayer.current_animation != "attack1":
+		animPlayer.play("attack1")
+
 func attack2_state():
-	animPlayer.play("atack2")
-	await animPlayer.animation_finished
-	state = MOVE
-	
-func combo1 ():
-	combo = true
-	await animPlayer.animation_finished
-	combo = false
+	velocity.x = 0
+	if not animPlayer.has_animation("attack2"): return
+	if animPlayer.current_animation != "attack2":
+		animPlayer.play("attack2")
+
+# --- НОВАЯ ФУНКЦИЯ ДЛЯ СОСТОЯНИЯ СМЕРТИ ---
+func death_state():
+	velocity.x = 0 # Труп не должен скользить
+
+	# Запускаем анимацию только один раз
+	if animPlayer.current_animation != "death":
+		animPlayer.play("death")
+
+	# Здесь нет проверки кнопок, поэтому игрок не может двигаться
+	# Ждем, пока сработает сигнал _on_animation_finished
